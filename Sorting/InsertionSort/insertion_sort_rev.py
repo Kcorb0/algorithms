ef069599a8ba29b5c4ca3736ae8c19b0ccbd2b9c
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-09-12 23:49:29 +0100

    reversed insertion sort

---
+++ Sorting/InsertionSort/insertion_sort_rev.py
@@ -0,0 +1,25 @@
+import random as rd
+from sorting_vis import vis_print
+import time
+
+
+def insertion_sort_reversed(arr):
+    for i in range(1, len(arr)):
+        # Initialize the starting key and set variable
+        # for prior key index
+        key = arr[i]
+        j = i - 1
+        # Move any elements that are less than the key
+        # one index position ahead of their current pos
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+inpsize = 100
+input_arr = list(range(inpsize))
+rd.shuffle(input_arr)
+
+print(insertion_sort_reversed(input_arr))
